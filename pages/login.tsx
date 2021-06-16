c57ac67255ade5b09594d536f9a6748db5f12b01
pllearns <phill.lorenzo@outlook.com>
2021-06-15 22:13:20 -0700

    feat: add firebase and quick pages

---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Login() {
+  return (
+    <div>
+      <p>This is the sign in</p>
+      <Link href="/">Back Home</Link>
+    </div>
+  )
+}
---
+++ pages/signup.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Signup() {
+  return (
+    <div>
+      <p>This is the sign up</p>
+      <Link href="/">Back Home</Link>
+    </div>
+  )
+}
